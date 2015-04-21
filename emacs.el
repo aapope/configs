db1bc3a277a5c417369f2c391d6337be43959c43
Andrew Pope <andrew.pope@emerson.com>
2015-04-21 11:18:08 -0500

    Cleaned up and re-organized Emacs config.

--- emacs.el
+++ emacs.el
@@ -11,51 +11,38 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+;; set custom key codes
 (global-set-key "\C-h" 'backward-delete-char)
-(add-to-list 'load-path "/home/apope/.emacs.d/auto-save-list")
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+
+;; remove backup files
 (setq make-backup-files nil)
+
+;; wrap lines
 (global-visual-line-mode 1)
 
-;; add newer version of org-mode
-;; (add-to-list 'load-path "~/.emacs.d/org-8.2.5h/lisp")
-;; (add-to-list 'load-path "~/.emacs.d/org-8.2.5h/contrib/lisp")
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-;; enable python for in-buffer evaluation
-(org-babel-do-load-languages
-'org-babel-load-languages
-'((python . t)
-  (java . t)))
-
-;; (menu-bar-mode -1)
+;; for non-terminal-based emacs
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; use theme
+;; set visual options
 (load-theme 'tango-dark)
+(set-default-font "Fantasque Sans Mono-13")
 
-;; package manager
+;; set package manager archives
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 						 ("marmalade" . "https://marmalade-repo.org/packages/")
 						 ("melpa" . "http://melpa.org/packages/")
 						 ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
-	  
-;; jedi for python mode
-(add-hook 'python-mode-hook 'jedi:setup)
-;; jedi for ipython mode
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
 
-;; change the default font
-(set-default-font "Fantasque Sans Mono-13")
 
-;; Checks list of required packages, installs if not available
+;; checks list of required packages, installs if not available
 (require 'cl)
 (defvar prelude-packages
   '(org jedi jedi-core ein)
@@ -77,4 +64,19 @@
 	  (package-install p))))
 
 (provide 'prelude-packages)
-;; end package updates
+;; end package installs
+
+;; custom keys for org-mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+;; enable python for in-buffer evaluation
+(org-babel-do-load-languages
+'org-babel-load-languages
+'((python . t)
+  (java . t)))
+
+;; jedi for python mode
+(add-hook 'python-mode-hook 'jedi:setup)
+;; jedi for ipython mode
+(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
