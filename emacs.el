ef1456fe4c9fb0b46ac58dc0776afe8025238b5c
Andrew Pope <andrew.pope@emerson.com>
2015-04-21 11:50:33 -0500

    Added handling for no package manager.

--- emacs.el
+++ emacs.el
@@ -35,52 +35,52 @@
 (if (display-graphic-p)
 	(load-gui-options))
 
-;; set package manager archives
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-						 ("marmalade" . "https://marmalade-repo.org/packages/")
-						 ("melpa" . "http://melpa.org/packages/")
-						 ("org" . "http://orgmode.org/elpa/")))
-(package-initialize)
-
-
-;; checks list of required packages, installs if not available
-(require 'cl)
-(defvar prelude-packages
-  '(org jedi jedi-core ein)
-  "A list of packages that need to be installed at launch.")
-
-(defun prelude-packages-installed-p ()
-  (loop for p in prelude-packages
-		when (not (package-installed-p p)) do (return nil)
-		finally (return t)))
-
-(unless (prelude-packages-installed-p)
-  ;; check for new packages (packages versions)
-  (message "%s" "Emacs is now refreshing package database...")
-  (package-refresh-contents)
-  (message "%s" "done")
-  ;; install missing packages
-  (dolist (p prelude-packages)
+;; if we have a package manager, install default
+;; packages and customize
+(when (require 'package nil 'noerror)
+  ;; set package manager archives
+  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+						   ("marmalade" . "https://marmalade-repo.org/packages/")
+						   ("melpa" . "http://melpa.org/packages/")
+						   ("org" . "http://orgmode.org/elpa/")))
+  (package-initialize)
+  
+  
+  ;; checks list of required packages, installs if not available
+  (require 'cl)
+  (defvar prelude-packages
+	'(org jedi jedi-core ein)
+	"A list of packages that need to be installed at launch.")
+  
+  (defun prelude-packages-installed-p ()
+	(loop for p in prelude-packages
+		  when (not (package-installed-p p)) do (return nil)
+		  finally (return t)))
+  
+  (unless (prelude-packages-installed-p)
+	;; check for new packages (packages versions)
+	(message "%s" "Emacs is now refreshing package database...")
+	(package-refresh-contents)
+	(message "%s" "done")
+	;; install missing packages
+	(dolist (p prelude-packages)
 	(when (not (package-installed-p p))
 	  (package-install p))))
 
-(provide 'prelude-packages)
-;; end package installs
-
-;; custom keys for org-mode
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-;; enable python for in-buffer evaluation
-(org-babel-do-load-languages
-'org-babel-load-languages
-'((python . t)
-  (java . t)))
-
-;; jedi for python mode
-(add-hook 'python-mode-hook 'jedi:setup)
-;; jedi for ipython mode
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
-
-(print window-system)
+  (provide 'prelude-packages)
+  ;; end package installs
+  
+  ;; custom keys for org-mode
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
+  ;; enable python for in-buffer evaluation
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)
+	 (java . t)))
+  
+  ;; jedi for python mode
+  (add-hook 'python-mode-hook 'jedi:setup)
+  ;; jedi for ipython mode
+  (add-hook 'ein:connect-mode-hook 'ein:jedi-setup))
